ae4a8136bc2d14dc19c6b00929127e4a343743de
CarlosGaLo <carlosgarridolopezons@gmail.com>
2022-08-24 11:29:03 +0200

    Some minor changes

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 // Una consulta simple para comprobar que la base de datos funciona
 // Puedes usar .select("id, title") para que solo devuelva campos específicos y no toda la tabla
 const supabase = createClient(
-  "https://gpebnufcyetzcbqhxqoh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwZWJudWZjeWV0emNicWh4cW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjEwMTU2MTEsImV4cCI6MTk3NjU5MTYxMX0.0X1d0jh_6Kjpj3Ax9zXTZ1-0FDREFtEOJYG7Gnc4Pfs"
+  "PON AQUÍ TU URL",
+  "PON AQUÍ TU CLAVE"
 );
 
 const articles = [];
